082483a348c8025261fbba8e9d52a5118393c2c2
Valentin1902 <162795289+Valentin1902@users.noreply.github.com>
2024-10-02 15:23:46 +0300

    =)

---
+++ main.js
@@ -0,0 +1,36 @@
+//
+
+// Завдання 2
+
+// class Circle {
+//   constructor(radius) {
+//     this.radius = radius;
+//   }
+//   getArea() {
+//     return Math.PI * Math.pow(this.radius, 2);
+//   }
+//   getCircumference() {
+//     return 2 * Math.PI * this.radius;
+//   }
+// }
+// const circle = new Circle(5);
+// console.log("Площа:", circle.getArea());
+// console.log("Периметр:", circle.getCircumference());
+
+//Завдання 3
+// class User {
+//   constructor(name, age, email) {
+//     this.name = name;
+//     this.age = age;
+//     this.email = email;
+//   }
+//   greet() {
+//     console.log(`Доброго дня, мене звати ${this.name}`);
+//   }
+//   isAdult() {
+//     return this.age >= 18;
+//   }
+// }
+// const user = new User("Олена", 20, "olena@gmail.com");
+// console.log(user.greet());
+// console.log("Чи повнолітня?", user.isAdult());
